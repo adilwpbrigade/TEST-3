4e6cd900c1b8d388309c23afce964e9ff2453b45
adilwpbrigade <adil.wpbrigade@gmail.com>
2025-12-01 13:44:56 +0500

    footer
    footer

--- WPB Test 3/assets/js/main.js
+++ WPB Test 3/assets/js/main.js
@@ -4,6 +4,31 @@ $(document).ready(function(){
       $("body").toggleClass("no-scroll");
     
   });
+   const swiper = new Swiper('.logoslider1', {
+                loop: true,
+                slidesPerView: 'auto', // Enables variable width per slide
+                spaceBetween: 40, // Increased spacing for better separation between auto-sized slides
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                // Responsive spacing adjustments (slidesPerView handled by 'auto')
+                breakpoints: {
+                    320: {
+                        spaceBetween: 20
+                    },
+                    768: {
+                        spaceBetween: 30
+                    },
+                    1024: {
+                        spaceBetween: 40
+                    }
+                }
+            });
 });
 
 
@@ -13,15 +38,16 @@ $(window).resize(function(){
   }
 });
 
-const logoSwiper = new Swiper('.logoSwiper', {
-  slidesPerView: '3',      
-  loop: true,                 
-  spaceBetween: 40,          
-  navigation: {
-    nextEl: '.logoswiper-button-next',
-    prevEl: '.logoswiper-button-prev',
-  },
-});
+// const logoSwiper = new Swiper('.logoSwiper1', {
+//   slidesPerView: '3',      
+//   loop: true,                 
+//   spaceBetween: 40,          
+//   navigation: {
+//     nextEl: '.logoswiper-button-next',
+//     prevEl: '.logoswiper-button-prev',
+//   },
+// });
+
 
 
 let mainSwiper = new Swiper(".main-swiper", {
